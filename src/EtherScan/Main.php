0eed20b911a46a631b6a7a0719f0ba9177c360df
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-09-05 19:13:23 +0400

    psr-4 package creation

---
+++ src/EtherScan/Main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harut
+ * Date: 9/5/17
+ * Time: 18:06
+ */
+
+namespace EtherScan;
+
+class Main
+{
+    public static function world()
+    {
+        return 'EtherScan Worked!';
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use EtherScan\Main;
+
+echo Main::world();
