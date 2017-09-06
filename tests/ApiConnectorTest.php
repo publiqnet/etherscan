b71d5249af46213f8b6d75a0f1d2ba78dcd9f844
haykgrigoryan <hayk.g@blockchainrndhub.com>
2017-09-06 17:00:00 +0400

    ApiConnectorTest.php create Sample test commit N1

--- tests/ApiConnectorTest.php
+++ tests/ApiConnectorTest.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 use EtherScan\ApiConnector;
 use PHPUnit\Framework\TestCase;
 
-define('API_KEY', 'testKey');
+define('API_KEY', 'BZ34DW4M5J6XZIQV5DWBC2MJV32V955Q1H');
 define('PREFIX', 'mod_api');
 
 class ApiConnectorTest extends TestCase
