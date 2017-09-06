f89b660a94956743589c865b37635f44bca3c787
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-09-06 16:26:02 +0400

    add absolute dir

--- examples/EtherScanExample.php
+++ examples/EtherScanExample.php
@@ -1,7 +1,7 @@
 <?php
 
-require '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$etherScan = new \EtherScan\EtherScan('BZ34DW4M5J6XZIQV5DWBC2MJV32V955Q1H');
+$etherScan = new EtherScan\EtherScan('BZ34DW4M5J6XZIQV5DWBC2MJV32V955Q1H');
 
 echo $etherScan->getStats()->getEthPrice();
