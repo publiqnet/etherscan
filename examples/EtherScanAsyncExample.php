790c00bfb388a1656a34c84ddcf9e5a5b32e3e32
sergeytangyan <sergeytangyan@gmail.com>
2017-09-07 15:51:56 +0400

    added more description in the readme

--- examples/EtherScanAsyncExample.php
+++ examples/EtherScanAsyncExample.php
@@ -5,7 +5,7 @@ use EtherScan\Resources\ApiConnector;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$esApiConnector = new ApiConnector('BZ34DW4M5J6XZIQV5DWBC2MJV32V955Q1H', EtherScan::MODE_API);
+$esApiConnector = new ApiConnector('your_api_key', EtherScan::MODE_API);
 $etherScan = new EtherScan($esApiConnector);
 
 echo $etherScan->getStats()->getEthPriceAsync(
